5e8c52200909769a8d5d836c90da7353d5bbd9be
Crowdbotics <team@crowdbotics.com>
2021-05-07 04:05:43 +0000

    Update models

--- backend/gogi_26309/wsgi.py
+++ backend/gogi_26309/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gogi_26309.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gogi_26309.settings")
 
 application = get_wsgi_application()
